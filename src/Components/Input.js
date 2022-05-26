be841794ebf366c9a86a5b796e0a7dd2637390b5
ldldz <qkrqudrjs4350@gmail.com>
2022-05-26 22:24:00 +0900

    feat: IdentityAuthentication에 useForm 적용

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,7 +1,20 @@
-export default function Input({ value, type, name, placeholder, children }) {
+export default function Input({
+  value,
+  type,
+  name,
+  placeholder,
+  children,
+  onChange,
+}) {
   return (
     <div className="inputWrapper">
-      <input value={value} type={type} name={name} placeholder={placeholder} />
+      <input
+        value={value}
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
       {children}
     </div>
   );
--- src/IdentityAuthentication.js
+++ src/IdentityAuthentication.js
@@ -1,9 +1,39 @@
 import { Input } from './components';
+import useForm from './hooks/useForm';
+
+const initialValues = {
+  mobileFirst: '010',
+  mobileMiddle: '',
+  mobileLast: '',
+  civilcodeFirst: '',
+  civilcodeLast: '',
+  name: '',
+};
+const onSubmit = async ({
+  name,
+  civilcodeFirst,
+  civilcodeLast,
+  mobileFirst,
+  mobileMiddle,
+  mobileLast,
+}) => {
+  const data = {
+    name,
+    civilcodeFirst,
+    civilcodeLast,
+    mobile: mobileFirst + mobileMiddle + mobileLast,
+  };
+  console.log(data); // post 요청으로 변경
+};
+const validate = () => ({});
 
 function IdentityAuthentication() {
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+  const { values, handleChange, handleSubmit } = useForm({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+
   return (
     <main>
       <h1>비대면 대출을 위해 본인 인증이 필요해요</h1>
@@ -11,26 +41,40 @@ function IdentityAuthentication() {
         <label>
           휴대폰 번호
           <div className="inputs">
-            <Input value={'010'} type={'tel'} name="phoneNumberFirst" />
+            <Input defaultValue={'010'} type={'tel'} name="mobileFirst" />
             -
-            <Input type={'tel'} name="phoneNumberMiddle" />
+            <Input
+              value={values.mobileMiddle}
+              type={'tel'}
+              name="mobileMiddle"
+              onChange={handleChange}
+            />
             -
-            <Input type={'tel'} name="phoneNumberLast" />
+            <Input
+              value={values.mobileLast}
+              type={'tel'}
+              name="mobileLast"
+              onChange={handleChange}
+            />
           </div>
         </label>
         <label>
           주민등록 번호
           <div className="inputs">
             <Input
+              value={values.civilcodeFirst}
               type={'number'}
-              name={'civilCodeFirst'}
+              name={'civilcodeFirst'}
               placeholder="앞 6자리"
+              onChange={handleChange}
             />
             -
             <Input
+              value={values.civilcodeLast}
               type={'password'}
-              name={'civilCodeLast'}
+              name={'civilcodeLast'}
               placeholder="뒤 7자리"
+              onChange={handleChange}
             />
           </div>
         </label>
@@ -38,9 +82,11 @@ function IdentityAuthentication() {
           이름
           <div className="inputs">
             <Input
+              value={values.name}
               type={'text'}
               name={'name'}
               placeholder="이름을 입력해 주세요"
+              onChange={handleChange}
             />
           </div>
         </label>

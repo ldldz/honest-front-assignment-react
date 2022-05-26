269db123104f01e612f1da72a03337adac57d1ab
ldldz <qkrqudrjs4350@gmail.com>
2022-05-27 01:36:40 +0900

    feat: useForm 수정
    - 현재 불필요한 error, loading 제거
    - handleChange에 pattern validation 추가
    - submit 시 validation -> form 입력시 validation 후 모든 입력이 유호하면 isValid true로 설정

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,32 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function useForm({ initialValues, onSubmit, validate }) {
   const [values, setValues] = useState(initialValues);
-  const [errors, setErrors] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [isValid, setIsValid] = useState(false);
 
-  const handleChange = ({ target: { name, value } }) =>
-    setValues({ ...values, [name]: value });
+  const handleChange = ({ target: { name, value, pattern } }) => {
+    if (value === '' || value.match(pattern) != null) {
+      const newValues = { ...values, [name]: value };
+      setValues(newValues);
+      setIsValid(Object.keys(validate(newValues)).length === 0);
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setIsLoading(true);
-    setErrors(validate(values));
+    onSubmit(values);
   };
-
-  useEffect(() => {
-    if (isLoading) {
-      if (Object.keys(errors).length === 0) {
-        onSubmit(values);
-      }
-      setIsLoading(false);
-    }
-  }, [errors]);
 
   return {
     values,
-    errors,
-    isLoading,
+    isValid,
     handleChange,
     handleSubmit,
   };

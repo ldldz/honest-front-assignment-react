7cc2fab6f29d899528572aaaedf90b1a416b0be3
ldldz <qkrqudrjs4350@gmail.com>
2022-05-30 00:50:17 +0900

    feat: 카운트다운 타이머 컴포넌트 생성

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,3 +1,34 @@
-export default function Timer() {
-  return <div className="timer">3:00</div>;
+import { useEffect, useState } from 'react';
+
+export default function Timer({ validTime, startTime, setIsTimerEnd }) {
+  const endTime = startTime + validTime;
+
+  const [time, setTime] = useState(
+    new Date(endTime).getTime() - new Date().getTime()
+  );
+
+  useEffect(() => {
+    if (endTime > new Date().getTime()) {
+      const counter = setInterval(() => {
+        const newTime = new Date(endTime).getTime() - new Date().getTime();
+        if (newTime < 0) {
+          setIsTimerEnd(true);
+          setTime(0);
+        } else {
+          setTime(newTime);
+        }
+      }, 1000);
+
+      return () => clearInterval(counter);
+    }
+  }, [time, startTime]);
+
+  function formatDate(date) {
+    const minutes = new Date(date).getMinutes().toString();
+    const seconds = new Date(date).getSeconds().toString().padStart(2, '0');
+
+    return `${minutes}:${seconds}`;
+  }
+
+  return <div className="timer">{formatDate(time)}</div>;
 }
